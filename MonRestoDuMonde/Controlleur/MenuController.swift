28567b659a02018fbaf5dd8995aa0bf967b578f4
Julien <julien@MacBook-Pro-de-Julien.local>
2018-08-27 15:03:30 +0200

    Ajout des Controlleurs principaux

---
+++ MonRestoDuMonde/Controlleur/MenuController.swift
@@ -0,0 +1,19 @@
+//
+//  MenuController.swift
+//  MonRestoDuMonde
+//
+//  Created by Julien on 27/08/2018.
+//  Copyright © 2018 Julien. All rights reserved.
+//
+
+import UIKit
+
+class MenuController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
