e2b76d3cefefc32737816314b5d13a1d772f85b9
Julien <julien@MacBook-Pro-de-Julien.local>
2018-08-28 13:58:10 +0200

    Ajout TabBarCustum

---
+++ MonRestoDuMonde/Controlleur/MonTabBar.swift
@@ -0,0 +1,18 @@
+//
+//  MonTabBar.swift
+//  MonRestoDuMonde
+//
+//  Created by Julien on 28/08/2018.
+//  Copyright © 2018 Julien. All rights reserved.
+//
+
+import UIKit
+
+class MonTabBar: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tabBar.barTintColor = GRIS_TRES_FONCE
+        tabBar.tintColor = GRIS_TRES_CLAIR
+    }
+}
---
+++ MonRestoDuMonde/Utilitaires/Constantes.swift
@@ -0,0 +1,14 @@
+//
+//  Constantes.swift
+//  MonRestoDuMonde
+//
+//  Created by Julien on 28/08/2018.
+//  Copyright © 2018 Julien. All rights reserved.
+//
+
+import UIKit
+
+let NOIR = UIColor.black
+let GRIS_TRES_FONCE = UIColor(red: 30/255, green: 30/255, blue: 30/255, alpha: 1)
+let GRIS_TRES_CLAIR = UIColor(red: 240/255, green: 240/255, blue: 240/255, alpha: 1)
+
